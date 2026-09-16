bazel-contrib/buildtools

commit d2a20dc9d54727597d66fd091cb16711375cac2b

Complete support for Starlark type annotations (#1488)
This change introduces parsing, AST, formatting, and linting support for
experimental Bazel Starlark type annotations
(https://github.com/bazelbuild/bazel/issues/27370) and Buck2 type
syntax:

- Type alias statements (`type OptionalDict[T, U] = dict[T, U] | None`).
- Generic `def` statements (`def foo[T, U](x: T) -> U`).
- Var statements (`x: list[int]`)
- `type_*` AST nodes to cleanly distinguish type syntax from standard
expressions. In particular, this allows us to improve formatting for
complex type applications, and to allow the ellipsis (`...`) only in
type expressions.

Not supported yet: `cast` and `isinstance` expressions - these will be
added in a follow-up PR; supporting them is a bit tricky due to the
limitations of yacc's LALR(1) grammar.

Disclosure: Gemini-assisted.

TAG=agy
CONV=ae2ff586-d57d-4fd4-a22a-883aec5d0802

## Buildtools PR checklist

- [x] The code in this PR is covered by unit/integration tests.
- [x] I have tested these changes and provide testing instructions
below.
- [ ] I have either responded to, or resolved all Gemini comments on the
PR.
- [ ] I have read Google Eng Practices on [Small
Changes](https://google.github.io/eng-practices/review/developer/small-cls.html),
this PR either follows these guidelines or the description provides
reasoning for why they can not be followed.

@brandjon @susinmotion FYI

warn/warn_control_flow.go
@@ -119,8 +119,8 @@ func findUnreachableStatements(stmts []build.Expr, callback func(build.Expr)) bo
 	for _, stmt := range stmts {
 		if unreachable {
 			if _, ok := stmt.(*build.CommentBlock); ok {
-    			continue
-    		}
+				continue
+			}
 			callback(stmt)
 			return true
 		}
@@ -188,7 +188,8 @@ func noEffectStatementsCheck(body []build.Expr, isTopLevel, isFunc bool, finding
 		}
 		switch s := (stmt).(type) {
 		case *build.DefStmt, *build.ForStmt, *build.IfStmt, *build.LoadStmt, *build.ReturnStmt,
-			*build.CallExpr, *build.CommentBlock, *build.BranchStmt, *build.AssignExpr:
+			*build.CallExpr, *build.CommentBlock, *build.BranchStmt, *build.AssignExpr,
+			*build.TypeAliasStmt, *build.TypedIdent:
 			continue
 		case *build.Comprehension:
 			if !isTopLevel || s.Curly {
@@ -238,13 +239,13 @@ func noEffectWarning(f *build.File) []*LinterFinding {
 //
 // Statements that contain other statements (for-loops, if-else blocks) are not
 // traversed inside.
-func extractIdentsFromStmt(stmt build.Expr) (assigned, used map[*build.Ident]bool) {
-	// The values for `assigned` are `true` if the warning for the variable should
+func extractIdentsFromStmt(stmt build.Expr) (defined, used map[*build.Ident]bool) {
+	// The values for `defined` are `true` if the warning for the variable should
 	// be suppressed, and `false` otherwise.
 	// It's still important to know that the variable has been assigned in the
 	// current scope because it could shadow a variable with the same name from an
 	// outer scope.
-	assigned = make(map[*build.Ident]bool)
+	defined = make(map[*build.Ident]bool)
 	used = make(map[*build.Ident]bool)
 
 	// Local scopes for comprehensions
@@ -304,7 +305,7 @@ func extractIdentsFromStmt(stmt build.Expr) (assigned, used map[*build.Ident]boo
 			}
 
 			for _, lValue := range bzlenv.CollectLValues(expr.LHS) {
-				assigned[lValue] = hasUnusedComment ||
+				defined[lValue] = hasUnusedComment ||
 					(!allLValuesUnderscored && strings.HasPrefix(lValue.Name, "_"))
 			}
 
@@ -322,7 +323,7 @@ func extractIdentsFromStmt(stmt build.Expr) (assigned, used map[*build.Ident]boo
 			//     for _, (_b, c) in iterable:
 			//         print(c)
 			for _, lValue := range bzlenv.CollectLValues(expr.Vars) {
-				assigned[lValue] = strings.HasPrefix(lValue.Name, "_")
+				defined[lValue] = strings.HasPrefix(lValue.Name, "_")
 			}
 
 			// Don't traverse inside the inner statements (but still traverse into
@@ -380,19 +381,33 @@ func extractIdentsFromStmt(stmt build.Expr) (assigned, used map[*build.Ident]boo
 			}
 			used[expr] = true
 
+		case *build.TypedIdent:
+			ident := expr.GetIdent()
+			if _, ok := defined[ident]; ok {
+				// If the same ident (not the same variable but the same AST node)
+				// is already registered as defined, it means that our TypedIdent
+				// isn't a var statemet, but the LHS of an AssignExpr that already
+				// processed its lvalues. Nothing left to do here.
+				return
+			}
+			// Var statements declare only 1 variable, so unlike for AssignExpr,
+			// we don't need to check for allLValuesUnderscored.
+			hasUnusedComment := edit.ContainsComments(expr, "@unused")
+			defined[ident] = hasUnusedComment
+
 		default:
 			// Do nothing, just traverse further
 		}
 		return
 	})
 
-	for ident := range assigned {
+	for ident := range defined {
 		// If the same ident (not the same variable but the same AST node) is
 		// registered as both "assigned" and "used", it means it was in fact just
 		// assigned, remove it from "used".
 		delete(used, ident)
 	}
-	return assigned, used
+	return defined, used
 }
 
 // unusedVariableCheck checks for unused variables inside a given node `stmt` (either *build.File or
@@ -402,10 +417,15 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 	findings := []*LinterFinding{}
 
 	// Symbols that are defined in the current scope
-	definedSymbols := make(map[string]*build.Ident)
+	definedSymbols := make(map[string]build.Expr)
 
 	// Functions that are defined in the current scope
-	definedFunctions := make(map[string]*build.DefStmt)
+	definedFunctions := make(map[string]build.Expr)
+
+	// For a file top-level, this is the map of type aliases.
+	// For a generic function, this is the scope (logically outer to the function's
+	// own scope) that holds the function's type parameters.
+	definedTypes := make(map[string]build.Expr)
 
 	// Symbols that are used in the current and inner scopes
 	usedSymbols := make(map[string]bool)
@@ -443,7 +463,16 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 			}
 
 			// The function is a root for the current scope.
-			// Collect its parameters as defined in the current scope.
+			// Collect its type parameters.
+			if expr.TypeParams != nil {
+				for _, typeParam := range bzlenv.CollectLValues(expr.TypeParams) {
+					definedTypes[typeParam.Name] = typeParam
+					if strings.HasPrefix(typeParam.Name, "_") || edit.ContainsComments(typeParam, "@unused") {
+						suppressedWarnings[typeParam.Name] = true
+					}
+				}
+			}
+			// Collect its (normal) parameters as defined in the current scope.
 			for _, param := range expr.Params {
 				// Function parameters are defined in the current scope.
 				if ident, _ := build.GetParamIdent(param); ident != nil {
@@ -456,7 +485,14 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 						// even if not used.
 						suppressedWarnings[ident.Name] = true
 					}
+
+					// Parameters' types are defined in type params or in the outer scope.
+					paramType := getParamType(param)
+					if paramType != nil {
+						collectUsedTypeSymbols(paramType, definedTypes, usedSymbols, usedSymbolsFromOuterScope)
+					}
 				}
+
 				// The default variables for the parameters are defined in the outer
 				// scope but used here.
 				assign, ok := param.(*build.AssignExpr)
@@ -473,6 +509,44 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 					usedSymbolsFromOuterScope[ident.Name] = true
 				}
 			}
+			// Return type may be defined in type params or in the outer scope.
+			if expr.Type != nil {
+				collectUsedTypeSymbols(expr.Type, definedTypes, usedSymbols, usedSymbolsFromOuterScope)
+			}
+
+		case *build.TypeAliasStmt:
+			if len(stack) > 0 {
+				// Same as for def statements, call unusedVariableCheck recursively to handle
+				// type parameter scope properly.
+
+				// The type alias name is defined in the current scope
+				typeName := expr.GetIdent().Name
+				if _, ok := definedTypes[typeName]; !ok {
+					definedTypes[typeName] = expr.Name
+				}
+				if edit.ContainsComments(expr, "@unused") {
+					suppressedWarnings[typeName] = true
+				}
+
+				// TODO: if Bazel/Buck2 ever support recursive type aliases, we'll want to
+				// check for type aliases that are only used in their own definition.
+				usedSymbolsInTypeAlias, findingsInTypeAlias := unusedVariableCheck(f, expr)
+				findings = append(findings, findingsInTypeAlias...)
+				for symbol := range usedSymbolsInTypeAlias {
+					usedSymbols[symbol] = true
+				}
+				return &build.StopTraversalError{}
+			}
+
+			if expr.TypeParams != nil {
+				for _, typeParam := range bzlenv.CollectLValues(expr.TypeParams) {
+					definedTypes[typeParam.Name] = typeParam
+					if strings.HasPrefix(typeParam.Name, "_") || edit.ContainsComments(typeParam, "@unused") {
+						suppressedWarnings[typeParam.Name] = true
+					}
+				}
+			}
+			collectUsedTypeSymbols(expr.Type, definedTypes, usedSymbols, usedSymbolsFromOuterScope)
 
 		case *build.LoadStmt:
 			// LoadStmt nodes store the loaded symbols as idents, even though in the
@@ -482,12 +556,12 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 			return
 
 		default:
-			assigned, used := extractIdentsFromStmt(expr)
+			defined, used := extractIdentsFromStmt(expr)
 
 			for symbol := range used {
 				usedSymbols[symbol.Name] = true
 			}
-			for symbol, isSuppressed := range assigned {
+			for symbol, isSuppressed := range defined {
 				if _, ok := definedSymbols[symbol.Name]; !ok {
 					definedSymbols[symbol.Name] = symbol
 					if isSuppressed {
@@ -505,6 +579,9 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 	// Collect variables that are used in the current or inner scopes but are not
 	// defined in the current scope.
 	for symbol := range usedSymbols {
+		if _, ok := definedTypes[symbol]; ok {
+			continue
+		}
 		if _, ok := definedSymbols[symbol]; ok {
 			continue
 		}
@@ -519,7 +596,51 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 	// Do not warn on exportable variables.
 	ignoreTopLevel := (f.Type == build.TypeBzl || f.Type == build.TypeDefault) && root == f
 
-	for name, ident := range definedSymbols {
+	findings = appendUnusedVariableLinterFindings(findings, definedTypes, "Type", usedSymbols, suppressedWarnings, ignoreTopLevel)
+	findings = appendUnusedVariableLinterFindings(findings, definedSymbols, "Variable", usedSymbols, suppressedWarnings, ignoreTopLevel)
+	findings = appendUnusedVariableLinterFindings(findings, definedFunctions, "Function", usedSymbols, suppressedWarnings, ignoreTopLevel)
+
+	return usedSymbolsFromOuterScope, findings
+}
+
+// Return the type annotation of a function parameter, or nil if there is none.
+func getParamType(param build.Expr) build.Expr {
+	switch param := param.(type) {
+	case *build.TypedIdent:
+		return param.Type
+	case *build.AssignExpr:
+		return getParamType(param.LHS)
+	case *build.UnaryExpr:
+		if param.Op == "*" || param.Op == "**" {
+			return getParamType(param.X)
+		}
+		return nil
+	default:
+		return nil
+	}
+}
+
+// Collect identifiers into a type expression into appropriate used-symbol maps, depending on whether
+// the identifier was defined as a type parameter in the given type param scope or not.
+//
+// Helper function for unusedVariableCheck.
+func collectUsedTypeSymbols(expr build.Expr, typeParamScope map[string]build.Expr, usedSymbolsFromTypeParams map[string]bool, usedSymbolsFromOuterScope map[string]bool) {
+	_, used := extractIdentsFromStmt(expr)
+	for ident := range used {
+		_, ok := typeParamScope[ident.Name]
+		if ok {
+			usedSymbolsFromTypeParams[ident.Name] = true
+		} else {
+			usedSymbolsFromOuterScope[ident.Name] = true
+		}
+	}
+}
+
+// Format and append linter findings for unused variables to the findings slice.
+//
+// Helper function for unusedVariableCheck.
+func appendUnusedVariableLinterFindings(findings []*LinterFinding, definedSymbols map[string]build.Expr, symbolKind string, usedSymbols map[string]bool, suppressedWarnings map[string]bool, ignoreTopLevel bool) []*LinterFinding {
+	for name, expr := range definedSymbols {
 		if _, ok := usedSymbols[name]; ok {
 			// The variable is used either in this scope or in a nested scope
 			continue
@@ -531,27 +652,16 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 		if ignoreTopLevel && !strings.HasPrefix(name, "_") {
 			continue
 		}
-		findings = append(findings,
-			makeLinterFinding(ident, fmt.Sprintf(`Variable %q is unused. Please remove it.`, ident.Name)))
-	}
-
-	for name, def := range definedFunctions {
-		if _, ok := usedSymbols[name]; ok {
-			// The function is used either in this scope or in a nested scope
-			continue
-		}
-		if ignoreTopLevel && !strings.HasPrefix(name, "_") {
-			continue
-		}
-		if _, ok := suppressedWarnings[name]; ok {
-			// The function is explicitly marked with @unused, ignore
+		if name == "_" {
+			// The "_" name is a strong signal that the variable is intentionally unused;
+			// it's also part of the `_: Any  # Enable type checking` idiom.
 			continue
 		}
 		findings = append(findings,
-			makeLinterFinding(def, fmt.Sprintf(`Function %q is unused. Please remove it.`, def.Name)))
+			makeLinterFinding(expr, fmt.Sprintf(`%s %q is unused. Please remove it.`, symbolKind, name)))
 	}
 
-	return usedSymbolsFromOuterScope, findings
+	return findings
 }
 
 func unusedVariableWarning(f *build.File) []*LinterFinding {
@@ -561,33 +671,69 @@ func unusedVariableWarning(f *build.File) []*LinterFinding {
 
 func redefinedVariableWarning(f *build.File) []*LinterFinding {
 	findings := []*LinterFinding{}
-	definedSymbols := make(map[string]bool)
+	const (
+		DeclaredVariable = iota
+		AssignedVariable
+		TypeAlias
+	)
+	definedSymbols := make(map[string]int)
 
 	types := DetectTypes(f)
 	for _, s := range f.Stmt {
-		// look for all assignments in the scope
-		as, ok := s.(*build.AssignExpr)
-		if !ok {
-			continue
-		}
-		left, ok := as.LHS.(*build.Ident)
-		if !ok {
-			continue
-		}
-		if !definedSymbols[left.Name] {
-			definedSymbols[left.Name] = true
+		switch s := s.(type) {
+		case *build.AssignExpr:
+			left, ok := s.LHSIdent()
+			if !ok {
+				continue
+			}
+			if kind, ok := definedSymbols[left.Name]; !ok || kind == DeclaredVariable {
+				definedSymbols[left.Name] = AssignedVariable
+				continue
+			}
+
+			if s.Op == "+=" && (types[s.LHS] == List || types[s.RHS] == List) && definedSymbols[left.Name] == AssignedVariable {
+				// Not a reassignment, just appending to a list
+				continue
+			}
+
+			suffix := ""
+			if definedSymbols[left.Name] == TypeAlias {
+				suffix = " as a type. Redefining a type is incompatible with static type checking."
+			} else {
+				suffix = ". Redefining a global value is discouraged and will be forbidden in the future. Consider using a new variable instead."
+			}
+
+			findings = append(findings, makeLinterFinding(s.LHS, fmt.Sprintf("Variable %q has already been defined%s", left.Name, suffix)))
+
+		case *build.TypedIdent:
+			ident := s.GetIdent()
+			if _, ok := definedSymbols[ident.Name]; !ok {
+				definedSymbols[ident.Name] = DeclaredVariable
+				continue
+			}
+			// TODO(#1500): don't report a warning for a var statement that redeclares a global variable because
+			// 1. this cannot be a warning that could be ignored - it is a static fatal error; and
+			// 2. it must be a fatal error for local variables too (not just globals).
+			// To fix, we need a proper resolver stage after parsing. See https://github.com/bazel-contrib/buildtools/issues/1500
+
+		case *build.TypeAliasStmt:
+			typeName := s.GetIdent().Name
+			if _, ok := definedSymbols[typeName]; !ok {
+				definedSymbols[typeName] = TypeAlias
+				continue
+			}
+
+			detail := ""
+			if definedSymbols[typeName] != TypeAlias {
+				detail = " as a variable"
+			}
+			findings = append(findings,
+				makeLinterFinding(s.Name, fmt.Sprintf("Type %q has already been defined%s. Redefining a type is incompatible with static type checking.", typeName, detail)))
+
+		default:
 			continue
 		}
 
-		if as.Op == "+=" && (types[as.LHS] == List || types[as.RHS] == List) {
-			// Not a reassignment, just appending to a list
-			continue
-		}
-
-		findings = append(findings,
-			makeLinterFinding(as.LHS, fmt.Sprintf(`Variable %q has already been defined. 
-Redefining a global value is discouraged and will be forbidden in the future.
-Consider using a new variable instead.`, left.Name)))
 	}
 	return findings
 }
@@ -713,7 +859,10 @@ func collectLocalVariables(stmts []build.Expr) []*build.Ident {
 			variables = append(variables, collectLocalVariables(stmt.False)...)
 		case *build.AssignExpr:
 			variables = append(variables, bzlenv.CollectLValues(stmt.LHS)...)
+		case *build.TypedIdent:
+			variables = append(variables, bzlenv.CollectLValues(stmt)...)
 		}
+		// Don't need to handle build.TypeAliasStmt - type alias statements must be top-level.
 	}
 	return variables
 }
@@ -773,6 +922,9 @@ func findUninitializedVariables(stmts []build.Expr, previouslyInitialized map[st
 				return &build.StopTraversalError{}
 			}
 			switch expr := expr.(type) {
+			case *build.TypedIdent:
+				// Don't traverse var statements: they declare variables, but don't assign any values.
+				return &build.StopTraversalError{}
 			case *build.DefStmt:
 				// The header of the DefStmt may contain uninitialized variables (e.g.
 				// default values of parameters) and should be traversed.

warn/warn_control_flow_test.go
@@ -704,6 +704,213 @@ sample_macro_with_used_foo()
 `,
 		[]string{},
 		scopeEverywhere)
+
+	// Unused variables declared by var statements
+	checkFindings(t, "unused-variable", `
+_x: int
+
+def foo():
+  y: str
+
+foo()
+`,
+		[]string{
+			":1: Variable \"_x\" is unused.",
+			":4: Variable \"y\" is unused.",
+		},
+		scopeEverywhere)
+
+	// Unused variables declared by var statements but marked @unused
+	checkFindings(t, "unused-variable", `
+# @unused
+_x: int
+
+def foo():
+  # @unused
+  y: str
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// "_" is always allowed to be unused
+	checkFindings(t, "unused-variable", `
+_: Any  # Enable type checking
+
+def foo():
+  _: None  # Enable type checking
+
+  def callback(_):
+    pass
+
+  callback(None)
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type alias used in function return annotation
+	checkFindings(t, "unused-variable", `
+type _Numeric = int | float
+
+def foo() -> _Numeric:
+  return 42
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type alias used in function parameter annotation
+	checkFindings(t, "unused-variable", `
+type _Numeric = int | float
+
+def foo(x: _Numeric):
+  print(x)
+
+foo(42)
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type alias unused - shadowed by function type parameter
+	checkFindings(t, "unused-variable", `
+type _Numeric = int | float
+
+def foo[_Numeric](x: _Numeric):
+  print(x)
+
+foo(42)
+`,
+		[]string{
+			":1: Type \"_Numeric\" is unused.",
+		},
+		scopeEverywhere)
+
+	// Type alias used in an assignment LHS
+	checkFindings(t, "unused-variable", `
+
+type _Numeric = int | float
+
+# @unused
+_x: _Numeric = 42
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type alias used in a var statement
+	checkFindings(t, "unused-variable", `
+
+type _Numeric = int | float
+
+# @unused
+_x: _Numeric
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type alias used in another type alias
+	checkFindings(t, "unused-variable", `
+
+type _Numeric = int | float
+
+# @unused
+type _OptionalNumeric = _Numeric | None
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type alias unused - shadowed by type alias type parameter
+	checkFindings(t, "unused-variable", `
+type _Numeric = int | float
+
+# @unused
+type _Opt[_T] = _T | None
+`,
+		[]string{
+			":1: Type \"_Numeric\" is unused.",
+		},
+		scopeEverywhere)
+
+	// Type alias explicitly marked @unused
+	checkFindings(t, "unused-variable", `
+# @unused
+type _Numeric = int | float
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Unused type alias
+	checkFindings(t, "unused-variable", `
+type _Numeric = int | float
+`,
+		[]string{
+			":1: Type \"_Numeric\" is unused.",
+		},
+		scopeEverywhere)
+
+	// Unused type parameter
+	checkFindings(t, "unused-variable", `
+def foo[
+	T,
+	_U,
+	# @unused
+	V,
+	]():
+  pass
+
+foo()
+`,
+		[]string{
+			":2: Type \"T\" is unused.",
+		},
+		scopeEverywhere)
+
+	// Type parameter used in parameter annotation
+	checkFindings(t, "unused-variable", `
+def foo[T](x: T):
+  print(x)
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type parameter used in parameter annotation with a default value
+	checkFindings(t, "unused-variable", `
+def foo[T](x: T = 42):
+  print(x)
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type parameter used in varargs/kwargs annotation
+	checkFindings(t, "unused-variable", `
+def foo[T, U](*args: T, **kwargs: U):
+  print(*args, **kwargs)
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
+
+	// Type parameter used in an inner function
+	checkFindings(t, "unused-variable", `
+def foo[T]():
+  def bar():
+    x: T = 42
+    print(x)
+
+  bar()
+
+foo()
+`,
+		[]string{},
+		scopeEverywhere)
 }
 
 func TestRedefinedVariable(t *testing.T) {
@@ -766,6 +973,45 @@ e += foo`,
 		[]string{
 			":15: Variable \"e\" has already been defined.",
 		},
+		scopeEverywhere)
+
+	checkFindings(t, "redefined-variable", `
+type T = int
+T = 42`,
+		[]string{
+			":2: Variable \"T\" has already been defined as a type.",
+		},
+		scopeEverywhere)
+
+	checkFindings(t, "redefined-variable", `
+type T = int
+T += [42]`,
+		[]string{
+			":2: Variable \"T\" has already been defined as a type.",
+		},
+		scopeEverywhere)
+
+	checkFindings(t, "redefined-variable", `
+T = 42
+type T = int`,
+		[]string{
+			":2: Type \"T\" has already been defined as a variable.",
+		},
+		scopeEverywhere)
+
+	checkFindings(t, "redefined-variable", `
+T: int
+type T = int`,
+		[]string{
+			":2: Type \"T\" has already been defined as a variable.",
+		},
+		scopeEverywhere)
+
+	// Ok to assign a variable after declaring it.
+	checkFindings(t, "redefined-variable", `
+T: int
+T = 42`,
+		[]string{},
 		scopeEverywhere)
 }
 
@@ -921,6 +1167,28 @@ def test(x: s1) -> List[s2]:
 `,
 		[]string{
 			":9: Loaded symbol \"s3\" is unused.",
+		},
+		scopeEverywhere)
+
+	checkFindingsAndFix(t, "load", `
+load(":f.bzl", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8")
+
+s1: s2
+s3: s4 = s5
+type s6[s7] = s8
+`,
+		`
+load(":f.bzl", "s2", "s4", "s5", "s8")
+
+s1: s2
+s3: s4 = s5
+type s6[s7] = s8
+`,
+		[]string{
+			"1: Loaded symbol \"s1\" is unused.",
+			"1: Loaded symbol \"s3\" is unused.",
+			"1: Loaded symbol \"s6\" is unused.",
+			"1: Loaded symbol \"s7\" is unused.",
 		},
 		scopeEverywhere)
 }
@@ -1334,6 +1602,25 @@ def foo():
   [x, y] = [1, 2]
   x = 3
   print(x)
+`,
+		[]string{},
+		scopeEverywhere)
+
+	checkFindings(t, "uninitialized", `
+def f():
+  x: int
+  return x
+`,
+		[]string{
+			":3: Variable \"x\" may not have been initialized.",
+		},
+		scopeEverywhere)
+
+	checkFindings(t, "uninitialized", `
+def f():
+  x: int
+  x = 1
+  return x
 `,
 		[]string{},
 		scopeEverywhere)

build/lex.go
@@ -297,17 +297,47 @@ func (in *input) parse() (f *File, err error) {
 	// Assign comments to nearby syntax.
 	in.assignComments()
 
+	if err := checkTypeAlias(in.file); err != nil {
+		return nil, err
+	}
+
 	return in.file, nil
+}
+
+// checkTypeAlias checks that type alias statements occur only at top-level.
+func checkTypeAlias(file *File) error {
+	var typeAliasErr error
+	WalkStatements(file, func(x Expr, stk []Expr) error {
+		if typeAliasErr != nil {
+			return &StopTraversalError{}
+		}
+		if typeAliasStmt, ok := x.(*TypeAliasStmt); ok && len(stk) > 1 {
+			typeAliasErr = ParseError{
+				Message:  "syntax error: type alias not at top level",
+				Filename: file.Path,
+				Pos:      typeAliasStmt.TypePos,
+			}
+			return &StopTraversalError{}
+		}
+		return nil
+	})
+	return typeAliasErr
 }
 
 // Error is called to report an error.
 // When called by the generated code s is always "syntax error".
 // Error does not return: it panics.
 func (in *input) Error(s string) {
+	in.ErrorAt(in.pos, s)
+}
+
+// ErrorAt is called to report an error at a specific position.
+// ErrorAt does not return: it panics.
+func (in *input) ErrorAt(pos Position, s string) {
 	if s == "syntax error" && in.lastToken != "" {
 		s += " near " + in.lastToken
 	}
-	in.parseError = ParseError{Message: s, Filename: in.filename, Pos: in.pos}
+	in.parseError = ParseError{Message: s, Filename: in.filename, Pos: pos}
 	panic(in.parseError)
 }
 
@@ -533,7 +563,16 @@ func (in *input) Lex(val *yySymType) int {
 		in.readRune()
 		return c
 
-	case '.', ':', ';', ',': // single-char tokens
+	case '.':
+		in.readRune()
+		if bytes.HasPrefix(in.remaining, []byte("..")) {
+			in.readRune()
+			in.readRune()
+			return _ELLIPSIS
+		}
+		return '.'
+
+	case ':', ';', ',': // single-char tokens
 		in.readRune()
 		return c
 
@@ -774,7 +813,16 @@ func (in *input) order(v Expr) {
 	case *Ident:
 		// nothing
 	case *TypedIdent:
+		in.order(v.Ident)
 		in.order(v.Type)
+	case *TypeAppExpr:
+		in.order(v.Type)
+		for _, x := range v.Args {
+			in.order(x)
+		}
+		in.order(&v.End)
+	case *EllipsisExpr:
+		// nothing
 	case *BranchStmt:
 		// nothing
 	case *DotExpr:
@@ -850,9 +898,17 @@ func (in *input) order(v Expr) {
 			in.order(v.Result)
 		}
 	case *DefStmt:
+		if v.TypeParams != nil {
+			in.order(v.TypeParams)
+		}
 		for _, x := range v.Params {
 			in.order(x)
 		}
+		in.order(v.ParamsEnd)
+		if v.Type != nil {
+			in.order(v.Type)
+		}
+		in.order(v.ColonPos)
 		for _, x := range v.Body {
 			in.order(x)
 		}
@@ -873,6 +929,12 @@ func (in *input) order(v Expr) {
 		for _, s := range v.False {
 			in.order(s)
 		}
+	case *TypeAliasStmt:
+		in.order(v.Name)
+		if v.TypeParams != nil {
+			in.order(v.TypeParams)
+		}
+		in.order(v.Type)
 	}
 	if len(in.suffixComments) > 0 && v != nil {
 		in.post = append(in.post, v)

commit 756ca0a2e2c9043ac5ab978507c45786dabe8228

Fix @unused for last argument of function
Regressed in d2a20dc9d54727597d66fd091cb16711375cac2b

warn/warn_control_flow.go
@@ -473,13 +473,16 @@ func unusedVariableCheck(f *build.File, root build.Expr) (map[string]bool, []*Li
 				}
 			}
 			// Collect its (normal) parameters as defined in the current scope.
-			for _, param := range expr.Params {
+			finalParameterMarkedUnused := expr.ColonPos != nil && edit.ContainsComments(expr.ColonPos, "@unused")
+			for i, param := range expr.Params {
 				// Function parameters are defined in the current scope.
 				if ident, _ := build.GetParamIdent(param); ident != nil {
 					definedSymbols[ident.Name] = ident
-					if ident.Name == "name" || strings.HasPrefix(ident.Name, "_") || edit.ContainsComments(param, "@unused") {
+					if ident.Name == "name" || strings.HasPrefix(ident.Name, "_") || edit.ContainsComments(param, "@unused") ||
+						i == len(expr.Params)-1 && finalParameterMarkedUnused {
 						// Don't warn about function arguments if they start with "_"
-						// or explicitly marked with @unused.
+						// or explicitly marked with @unused. An @unused comment at the
+						// end of the function header applies to its final argument.
 						// Also don't warn about unused "name" arguments, it could be a
 						// macro where such argument is encouraged (by `unnamed-macro`)
 						// even if not used.

warn/warn_control_flow_test.go
@@ -655,6 +655,23 @@ foo()
 		scopeEverywhere)
 
 	checkFindings(t, "unused-variable", `
+def foo(x):  # @unused
+  pass
+
+def bar(
+    x,
+    y):  # @unused
+  pass
+
+foo()
+bar()
+`,
+		[]string{
+			":5: Variable \"x\" is unused.",
+		},
+		scopeEverywhere)
+
+	checkFindings(t, "unused-variable", `
 def foo(
     name,
     x):