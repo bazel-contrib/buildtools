e8ca2a7769ca2c69b826eae2a901066393a54a93
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-09-23 13:54:08 +0200

    Remove obsolete runfiles workaround

--- buildozer/module/tests/bcr/run_buildozer.sh
+++ buildozer/module/tests/bcr/run_buildozer.sh
@@ -4,9 +4,4 @@ set -o errexit -o nounset -o pipefail
 
 cd "$BUILD_WORKSPACE_DIRECTORY"
 
-# Workaround for https://github.com/bazelbuild/bazel/issues/17571
-unset RUNFILES_DIR
-unset RUNFILES_MANIFEST_FILE
-unset RUNFILES_MANIFEST_ONLY
-
 bazel run @buildozer -- 'dict_set env SHOULD_PASS:1' //bazel_run_test
